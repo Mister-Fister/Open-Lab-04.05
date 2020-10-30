4a2d0a32b6bb57a53640ac691e4c92ee4df55c41
Matúš <matusskor@gmail.com>
2020-10-30 10:40:26 +0100

    zmeny

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,18 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            
+            string a = "";
+            
+            for(int i = 0; i < orig.Length; i++)
+            {
+                for (int q = 0; q < n; q++)
+                {
+                    a = a + orig[i];
+                }
+            }
+            return a;
+            
         }
     }
 }
